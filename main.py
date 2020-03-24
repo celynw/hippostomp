f2579b87bd22f99a411d49fed75484ffa85c8a4a
Celyn Walters <celynwalters+gitlab@gmail.com>
2020-03-24 00:01:07 +0000

    Add main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import colored_traceback.auto
+import argparse
+from pathlib import Path
+
+from kellog import debug, info, warning, error
+
+# ==================================================================================================
+def main(args):
+	filePath = args.root_dir / "Data" / "AbuSimbel.sg3"
+	info(f"Opening {filePath}...")
+
+
+# ==================================================================================================
+def parse_args():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("root_dir", metavar="DIRECTORY", help="Location of Pharaoh installation directory")
+
+	args = parser.parse_args()
+	args.root_dir = Path(args.root_dir)
+
+	return args
+
+
+# ==================================================================================================
+if __name__ == "__main__":
+	main(parse_args())
